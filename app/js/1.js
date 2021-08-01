8ef558e3ac53d8b126272bc7d8d9c69844438bd9
huy <huy@gmail.com>
2021-08-01 17:37:03 +0700

    slide content

--- app/js/1.js
+++ app/js/1.js
@@ -1,3 +1,5 @@
+// let Flickity = require("Flickity");
+
 let time = setInterval(function () {
   autoSlide();
 }, 4000);
@@ -27,3 +29,34 @@ function autoSlide() {
     slides[0].classList.add("active");
   }
 }
+
+//slide// init Flickity
+var $carousel = $(".products .main-carousel").flickity({
+  prevNextButtons: false,
+  pageDots: false,
+});
+// Flickity instance
+var flkty = $carousel.data("flickity");
+// elements
+var $cellButtonGroup = $(".button-group--cells");
+var $cellButtons = $cellButtonGroup.find(".button");
+
+// update selected cellButtons
+$carousel.on("select.flickity", function () {
+  $cellButtons.filter(".is-selected").removeClass("is-selected");
+  $cellButtons.eq(flkty.selectedIndex).addClass("is-selected");
+});
+
+// select cell on button click
+$cellButtonGroup.on("click", ".button", function () {
+  var index = $(this).index();
+  $carousel.flickity("select", index);
+});
+// previous
+$(".button--previous").on("click", function () {
+  $carousel.flickity("previous", true);
+});
+// next
+$(".button--next").on("click", function () {
+  $carousel.flickity("next", true);
+});
